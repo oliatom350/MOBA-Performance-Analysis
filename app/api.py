745894e20d244b8f8eb61f56ba6a7a84baebd075
oliatom350 <trollins030@gmail.com>
2024-01-29 20:12:40 +0100

    Resolución parcial de error al almacenar partidas

--- app/api.py
+++ app/api.py
@@ -155,11 +155,13 @@ def getPlayerMatches(puuid, existing: bool):
             # Cuarto, tras comprobar la fecha de la última partida y habiendo procesado esas 100 partidas, volvemos a buscar
             # otros 100 IDs utilizando como endTime esta fecha
             # TODO Se queda en un bucle infinito cuando el jugador ya existe en la BBDD
-            result = getIDMatches(puuid, QueueType.Normal, limitAPIDate, endTime, 100)
+            result2 = getIDMatches(puuid, QueueType.Normal, limitAPIDate, endTime, 100)
             # Al haber comprobado que a veces la API devuelve IDs consecutivos de partidas no almacenadas, usamos un
             # flag para abandonar la búsqueda si esto ocurre
-            if exitGameSearch:
+            if exitGameSearch or result == result2:
                 break
+            else:
+                result = result2
     return newPlayers
     # En líneas generales, deberían procesarse todas las partidas disputadas por el jugador hasta el 16 de junio de
     # 2021, fecha en la que se almacenan las primeras partidas localizables con parámetros temporales.
@@ -237,9 +239,9 @@ def doRequest(APIurl):
     elif response.status_code == 429:
         waitTime = int(response.headers["retry-after"])
         print(f"Esperando {waitTime} segundos para continuar con la petición de partidas...")
-        time.sleep(waitTime/2 + 1)
+        time.sleep(int(waitTime/2))
         print(f"Esperando {int(waitTime - waitTime/2 + 1)} segundos para continuar con la petición de partidas...")
-        time.sleep(waitTime - waitTime/2 + 1)
+        time.sleep(int(waitTime - waitTime/2))
         response = requests.get(APIurl, headers=headers)
         return response.json()
     elif response.status_code == 404:
--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from app import api, database
 
 
 if __name__ == '__main__':
-    # database.clearCollection(0)
-    # database.clearCollection(1)
-    # database.clearCollection(2)
+    database.clearCollection(0)
+    database.clearCollection(1)
+    database.clearCollection(2)
     api.updateChampions()
     summoner_name = input("Introduce tu nombre de usuario: ")
     puuidInicial = api.getSummonerPUUID(summoner_name)
