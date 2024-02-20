ad41bdd9e6076cf1f4de0fda6d50d9788a025e09
oliatom350 <trollins030@gmail.com>
2024-02-20 11:33:33 +0100

    Funciones KDA y comienzo de función del winrate

--- app/proc_data.py
+++ app/proc_data.py
@@ -7,7 +7,9 @@ nGamesThreshold = 100
 
 def processPlayer(name):
     puuid = api.getSummonerPUUID(name)
-    getPlayerMatches(name, puuid)
+    matches = getPlayerMatches(name, puuid)
+    # getMatchesPosition(name, puuid, matches)
+    getPlayerKDA(name, puuid, matches)
 
 
 def getPlayerMatches(name, puuid):
@@ -32,7 +34,7 @@ def getPlayerMatches(name, puuid):
     # Una vez llegados a este punto, deberían haberse recuperado un número mínimo de 100 partidas totales.
     # En el caso de que no sean suficientes, se mostrará un mensaje de que no hay suficientes datos para analizar al jugador
     print(f'Se han recuperado {len(matches)} partidas de {name}')
-    getMatchesPosition(name, puuid, matches)
+    return matches
 
 
 def getMatchesPosition(name, puuid, matches):
@@ -104,20 +106,20 @@ def getMatchesPosition(name, puuid, matches):
         f"{totalFlex} partidas Ranked Flex ({perFlex}%)\n"
         f"{unknownQueue} partidas de otro tipo ({perUnknownQueue}%)\n"
     )
-    totalMatchesCount = totalNorm+totalSolo+totalFlex
-    perTop = round((topNorm+topSolo+topFlex * 100 / totalMatchesCount), 2)
-    perJg = round((jungleNorm+jungleSolo+jungleFlex * 100 / totalMatchesCount), 2)
-    perMid = round((midNorm+midSolo+midFlex * 100 / totalMatchesCount), 2)
-    perAdc = round((adcNorm+adcSolo+adcFlex * 100 / totalMatchesCount), 2)
-    perSupp = round((suppNorm+suppSolo+suppFlex * 100 / totalMatchesCount), 2)
+    totalMatchesCount = totalNorm + totalSolo + totalFlex
+    perTop = round(((topNorm + topSolo + topFlex) * 100 / totalMatchesCount), 2)
+    perJg = round(((jungleNorm + jungleSolo + jungleFlex) * 100 / totalMatchesCount), 2)
+    perMid = round(((midNorm + midSolo + midFlex) * 100 / totalMatchesCount), 2)
+    perAdc = round(((adcNorm + adcSolo + adcFlex) * 100 / totalMatchesCount), 2)
+    perSupp = round(((suppNorm + suppSolo + suppFlex) * 100 / totalMatchesCount), 2)
     perUnknownPosition = round((unknown * 100 / totalMatchesCount), 2)
     print(
         f"También ha jugado en las siguientes posiciones: \n"
-        f"Top: {topNorm+topSolo+topFlex} veces ({perTop} %)\n"
-        f"Jungla: {jungleNorm+jungleSolo+jungleFlex} veces ({perJg} %)\n"
-        f"Mid: {midNorm+midSolo+midFlex} veces ({perMid} %)\n"
-        f"Tirador: {adcNorm+adcSolo+adcFlex} veces ({perAdc} %)\n"
-        f"Support: {suppNorm+suppSolo+suppFlex} veces ({perSupp} %)\n"
+        f"Top: {topNorm + topSolo + topFlex} veces ({perTop} %)\n"
+        f"Jungla: {jungleNorm + jungleSolo + jungleFlex} veces ({perJg} %)\n"
+        f"Mid: {midNorm + midSolo + midFlex} veces ({perMid} %)\n"
+        f"Tirador: {adcNorm + adcSolo + adcFlex} veces ({perAdc} %)\n"
+        f"Support: {suppNorm + suppSolo + suppFlex} veces ({perSupp} %)\n"
         f"Línea desconocida: {unknown} veces ({perUnknownPosition} %)\n"
     )
 
@@ -140,6 +142,158 @@ def getMatchPlayerInfo(puuid, match):
         return participants[index]
     else:
         return None
+
+
+def getPlayerKDA(name, puuid, matches):
+    # El objetivo de esta función es obtener:
+    # - KDA total
+    # - KDA en cada partida
+    # - KDA total por tipo de cola
+    # - KDA por campeón
+    totaldeaths = totalkills = totalassists = deathsNormal = deathsSolo = deathsFlex = killsNormal = killsSolo = \
+        killsFlex = assistsNormal = assistsSolo = assistsFlex = totalMatchNormal = totalMatchSolo = totalMatchFlex = 0
+    # Creamos los diccionarios vacíos que incluirán las stats por campeones y por partidas
+    dicChamps = {}
+    dicMatches = {}
+    totalGames = len(matches)
+
+    for match in matches:
+        queueType = match['info']['queueId']
+        info = getMatchPlayerInfo(puuid, match)
+        if info is None:
+            continue
+        if queueType != 400 and queueType != 420 and queueType != 440:
+            continue
+        kills = info['kills']
+        deaths = info['deaths']
+        assists = info['assists']
+        champName = info['championName']
+        dicMatches[match['metadata']['matchId']] = [kills, deaths, assists]
+
+        # Este fragmento de suma se podría eliminar en el futuro, ya que solo se usa para evitar recorrer de nuevo
+        # dicMatches sumando cada estadística al total. dicMatches se puede usar para enviar o devolver la info por partida
+        totalkills += kills
+        totaldeaths += deaths
+        totalassists += assists
+
+        if champName in dicChamps:
+            dicChamps[champName][0] += kills
+            dicChamps[champName][1] += deaths
+            dicChamps[champName][2] += assists
+            dicChamps[champName][3] += 1
+        else:
+            dicChamps[champName] = [kills, deaths, assists, 1]
+        if queueType == 400:
+            # Procesamiento cola Normal
+            killsNormal += kills
+            deathsNormal += deaths
+            assistsNormal += assists
+            totalMatchNormal += 1
+        elif queueType == 420:
+            # Procesamiento cola Ranked Solo/Duo
+            killsSolo += kills
+            deathsSolo += deaths
+            assistsSolo += assists
+            totalMatchSolo += 1
+        elif queueType == 440:
+            # Procesamiento cola Ranked Flex
+            killsFlex += kills
+            deathsFlex += deaths
+            assistsFlex += assists
+            totalMatchFlex += 1
+
+    if totalGames != 0:
+        print(
+            f"El jugador {name} ha jugado {len(dicMatches)} partidas entre Normal, Solo/Duo y Flex, obteniendo los siguientes resultados:")
+
+        print(
+            f"KDA total: ({round(totalkills / totalGames, 2)} / {round(totaldeaths / totalGames, 2)} / {round(totaldeaths / totalGames, 2)}): "
+            f"{calculateKDA(totalkills, totaldeaths, totalassists)}")
+        print(f"\n\nKDA por colas:")
+        if totalMatchNormal != 0:
+            print(
+                f"NormalQueue: ({round(killsNormal / totalMatchNormal, 2)} / {round(deathsNormal / totalMatchNormal, 2)} / {round(assistsNormal / totalMatchNormal, 2)}): "
+                f"{calculateKDA(killsNormal, deathsNormal, assistsNormal)}")
+        else:
+            print(f"No se han obtenido resultados sobre partidas normales")
+        if totalMatchSolo != 0:
+            print(
+                f"Solo/Duo: ({round(killsSolo / totalMatchSolo, 2)} / {round(deathsSolo / totalMatchSolo, 2)} / {round(assistsSolo / totalMatchSolo, 2)}): "
+                f"{calculateKDA(killsSolo, deathsSolo, assistsSolo)}")
+        else:
+            print(f"No se han obtenido resultados sobre partidas solo/duo")
+        if totalMatchFlex != 0:
+            print(
+                f"Flex: ({round(killsNormal / totalMatchFlex, 2)} / {round(deathsNormal / totalMatchFlex, 2)} / {round(assistsNormal / totalMatchFlex, 2)}): "
+                f"{calculateKDA(killsFlex, deathsFlex, assistsFlex)}")
+        else:
+            print(f"No se han obtenido resultados sobre partidas flex")
+        print(f"\n\nKDA por campeones jugados:")
+        for champ in dict(sorted(dicChamps.items(), key=lambda x: x[1][3], reverse=True)):
+            print(
+                f"{champ}: ({round(dicChamps[champ][0] / dicChamps[champ][3], 2)} / {round(dicChamps[champ][1] / dicChamps[champ][3], 2)} / {round(dicChamps[champ][2] / dicChamps[champ][3], 2)}): "
+                f"{calculateKDA(dicChamps[champ][0], dicChamps[champ][1], dicChamps[champ][2])} en {dicChamps[champ][3]} partidas jugadas")
+    else:
+        print(
+            f"El jugador {name} no ha jugado ninguna partida")
+
+
+def calculateKDA(kills, deaths, assists):
+    if deaths == 0:
+        return round(kills + assists, 2)
+    else:
+        return round((kills + assists) / deaths, 2)
+
+
+def getPlayerWinrate(name, puuid, matches):
+    # El objetivo de esta función es obtener el winrate del jugador basándose en los siguientes criterios:
+    # - Winrate total
+    # - Winrate por cola
+    # - Winrate por posición
+    # - Winrate por campeón
+    # TODO Si se quiere comprobar el winrate de un campeón o de una posición en una cola concreta, modificar en el futuro la función
+    totalVictorias = totalDerrotas = winNormal = loseNormal = winSolo = loseSolo = winFlex = loseFlex = winUnknown = \
+        loseUnknown = winTop = loseTop = winJg = loseJg = winMid = loseMid = winAdc = loseAdc = winSupp = loseSupp = 0
+    for match in matches:
+        queueType = match['info']['queueId']
+        info = getMatchPlayerInfo(puuid, match)
+        win = info['win']
+        if info is None:
+            continue
+
+        if win:
+            totalVictorias += 1
+        else:
+            totalDerrotas += 1
+
+        if queueType != 400 and queueType != 420 and queueType != 440:
+            # Tratamiento otras colas
+            if win:
+                winUnknown += 1
+            else:
+                loseUnknown += 1
+        else:
+            if queueType == 400:
+                # Tratamiento Normal
+                if win:
+                    winNormal += 1
+                else:
+                    loseNormal += 1
+            elif queueType == 420:
+                # Tratamiento SoloDuo
+                if win:
+                    winSolo += 1
+                else:
+                    loseSolo += 1
+            elif queueType == 440:
+                # Tratamiento Flex
+                if win:
+                    winFlex += 1
+                else:
+                    loseFlex += 1
+
+
+
 
 # FUNCIONES ESTADÍSTICAS DESCRIPTIVAS
 
