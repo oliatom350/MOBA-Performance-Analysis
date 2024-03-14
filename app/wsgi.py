245a36d23e7b085f9287267b10006534c1cff6e9
oliatom350 <trollins030@gmail.com>
2024-03-14 09:11:22 +0100

    Creación del fichero referente a Flask

---
+++ app/wsgi.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/', methods=['POST', 'GET'])
+def hello():
+    if request.method == 'GET':
+        return '<p>Hello, World. This is GET request</p>'
+    else:
+        return '<p>Hello, World. This is POST request</p>'
+
+
+if __name__ == '__main__':
+    app.run()
